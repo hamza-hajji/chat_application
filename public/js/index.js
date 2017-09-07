ef8315aaa2ba0c9c230b68b41635d3991aa302b0
Hamza Hajji <hmzhjji@gmail.com>
2017-09-07 12:12:57 +0100

    emit event from server to all connected clients

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('connected to server');
-  socket.emit('createMessage', {
-    from: 'janine',
-    content: 'Is there anybody in there?'
-  });
 });
 socket.on('disconnect', function () {
   console.log('disconnected from server');
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'jack',
-    content: 'What are you up to?',
-    createdAt: 123
-  });
-
   socket.on('createMessage', (message) => {
     console.log('Create Message', message);
+    io.emit('newMessage', {
+      from: message.from,
+      content: message.content,
+      createdAt: Date.now()
+    });
   });
 
   socket.on('disconnect', () => {
